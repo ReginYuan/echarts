9ce2cad3912213d8d484ca9fb3d5e251b0d14673
ReginYuan <2496943860@qq.com>
2023-12-10 15:13:15 +0800

    feat: 上传项目

---
+++ echarts-views/src/views/Histogram.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="histogram" id="histogram"></div>
+</template>
+<script setup>
+import * as echarts from 'echarts'
+import { onMounted } from 'vue'
+
+const init = () => {
+  // 基于准备好的dom，初始化echarts实例
+  var myChart = echarts.init(document.getElementById('histogram'))
+  // 绘制图表
+  myChart.setOption({
+    title: {
+      text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    xAxis: {
+      type: 'category', //类目轴
+      data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+    },
+    yAxis: {
+      type: 'value' //数值轴
+    },
+    series: [
+      {
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }
+    ]
+  })
+}
+
+onMounted(() => {
+  init()
+})
+</script>
+<style scoped>
+.histogram {
+  width: 500px;
+  height: 500px;
+}
+</style>
---
+++ echarts-views/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Histogram from './Histogram.vue'
+</script>
+
+<template>
+  <div class="home_view">
+    <Histogram></Histogram>
+  </div>
+</template>
+
+<style scoped>
+.home_view {
+  height: 100%;
+  width: 100%s;
+}
+</style>
